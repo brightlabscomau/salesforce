15c043dc1d176be224c600ead4bc0190eff55347
Nipun Perera <nipun@brightlabs.com.au>
2024-03-25 13:26:51 +1100

    feat: push assignments to algolia

--- src/services/Assignment.php
+++ src/services/Assignment.php
@@ -4,10 +4,9 @@ namespace brightlabs\craftsalesforce\services;
 
 use brightlabs\craftsalesforce\elements\Assignment as ElementsAssignment;
 use Craft;
-use craft\helpers\Db;
-use craft\helpers\StringHelper;
-use Dompdf\FrameDecorator\Table;
 use yii\base\Component;
+use Algolia\AlgoliaSearch\SearchClient;
+use craft\helpers\App;
 
 /**
  * Assignment service
@@ -21,11 +20,51 @@ class Assignment extends Component
 
     public function saveAssignment(ElementsAssignment $assignment)
     {
+
+        if ($assignment->publish == 'AVP Portal (Public)') {
+            $this->createOnAlgolia($assignment);
+        }
+
         return Craft::$app->elements->saveElement($assignment, true);
     }
 
     public function deleteAssignment(ElementsAssignment $assignment)
     {
+        if ($assignment->publish == 'AVP Portal (Public)') {
+            $this->deleteOnAlgolia($assignment);
+        }
+
         return Craft::$app->elements->deleteElement($assignment, true);
+    }
+
+    protected function createOnAlgolia(ElementsAssignment $assignment)
+    {
+        if (empty(App::env('ALGOLIA_APPLICATION_ID'))) {
+            return;
+        }
+
+        $client = SearchClient::create(App::env('ALGOLIA_APPLICATION_ID'), App::env('ALGOLIA_ADMIN_API_KEY'));
+        $index = $client->initIndex(App::env('ALGOLIA_INDEX_ASSIGNMENTS'));
+
+        $assignmentArray = [
+            'objectID' => $assignment->salesforceId,
+            'title' => $assignment->title ?? '',
+            'country' => $assignment->country ?? '',
+            'duration' => $assignment->duration ?? '0',
+            'uri' => $assignment->uri ?? '',
+        ];
+
+        $index->saveObject($assignmentArray);
+    }
+
+    protected function deleteOnAlgolia(ElementsAssignment $assignment)
+    {
+        if (empty(App::env('ALGOLIA_APPLICATION_ID'))) {
+            return;
+        }
+
+        $client = SearchClient::create(App::env('ALGOLIA_APPLICATION_ID'), App::env('ALGOLIA_ADMIN_API_KEY'));
+        $index = $client->initIndex(App::env('ALGOLIA_INDEX_ASSIGNMENTS'));
+        $index->deleteObject($assignment->salesforceId);
     }
 }
